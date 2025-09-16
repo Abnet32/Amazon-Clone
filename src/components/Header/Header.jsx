89be5100774b031de40c92dd2527fc821091515b
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-09-16 08:07:47 -0400

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import React, { useContext } from "react";
 import logo from "../../assets/images/amazon_PNG11.png";
 import flag from "../../assets/images/flag.png";
 import { SlLocationPin } from "react-icons/sl";
@@ -7,23 +6,18 @@ import { LiaCartArrowDownSolid } from "react-icons/lia";
 
 import classes from "./Header.module.css";
 import LowerHeader from "./LowerHeader";
-// import { Link } from "react-router-dom";
-// import { DataContext } from "../DataProvider/DataProvider";
-// import { auth } from "../../Utility/firebase";
+
 const Header = () => {
-  // const [{ basket, user }, dispatch] = useContext(DataContext);
-  // const totalItem = basket?.reduce((amount, item) => {
-  //   return item.amount + amount;
-  // }, 0);
+ 
   return (
     <section className={classes.fixed}>
       <section>
         <div className={classes.header__container}>
           <div className={classes.logo__container}>
             {/* logo */}
-            <Link to="/">
+            <div to="/">
               <img src={logo} alt="amazon logo" />
-            </Link>
+            </div>
             <div className={classes.delivery}>
               {/* delivery */}
               <span>
@@ -47,7 +41,7 @@ const Header = () => {
           </div>
           {/* rigth-side */}
           <div className={classes.order__container}>
-            <Link
+            <div
               to="
             "
               className={classes.language}
@@ -57,8 +51,8 @@ const Header = () => {
               <select>
                 <option value="">EN</option>
               </select>
-            </Link>
-            <Link to={!user && "/auth"}>
+            </div>
+            <div to={!user && "/auth"}>
               <div>
                 {user ? (
                   <>
@@ -72,15 +66,15 @@ const Header = () => {
                   </>
                 )}
               </div>
-            </Link>
-            <Link to="/orders">
+            </div>
+            <div to="/orders">
               <p>Returns</p>
               <span>& Orders</span>
-            </Link>
-            <Link to="/cart" className={classes.cart}>
+            </div>
+            <div to="/cart" className={classes.cart}>
               <LiaCartArrowDownSolid size={35} />
-              <span>{totalItem}</span>
-            </Link>
+              <span></span>
+            </div>
           </div>
         </div>
       </section>
