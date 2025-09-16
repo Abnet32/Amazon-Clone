42daf0f61adf0fd4bfea9746e690484b6b9704da
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-09-15 23:32:48 -0400

    up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,90 +1,63 @@
-import { useContext } from "react";
 import logo from "../../assets/images/amazon_PNG11.png";
 import flag from "../../assets/images/flag.png";
 import { SlLocationPin } from "react-icons/sl";
 import { FaSearch } from "react-icons/fa";
 import { LiaCartArrowDownSolid } from "react-icons/lia";
-
 import classes from "./Header.module.css";
-import LowerHeader from "./LowerHeader";
-import { Link } from "react-router-dom";
-// import { DataContext } from "../DataProvider/DataProvider";
-// import { auth } from "../../Utility/firebase";
 const Header = () => {
-  const [{ basket, user }, dispatch] = useContext(DataContext);
-  const totalItem = basket?.reduce((amount, item) => {
-    return item.amount + amount;
-  }, 0);
   return (
     <section className={classes.fixed}>
       <section>
-        <div className={classes.header__container}>
-          <div className={classes.logo__container}>
-            {/* logo */}
-            <Link to="/">
-              <img src={logo} alt="amazon logo" />
-            </Link>
-            <div className={classes.delivery}>
-              {/* delivery */}
-              <span>
-                <SlLocationPin />
-              </span>
-              <div>
-                <p>Deliver to</p>
-                <span>Ethiopia</span>
-              </div>
-            </div>
-          </div>
-
-          <div className={classes.search}>
-            {/* search */}
-            <select name="" id="">
-              <option value="">All</option>
-            </select>
-            <input type="text" name="" id="" placeholder="search product" />
-            {/* icon */}
-            <FaSearch size={37} />
-          </div>
-          {/* rigth-side */}
-          <div className={classes.order__container}>
-            <Link
-              to="
-            "
-              className={classes.language}
-            >
-              {/* flag */}
-              <img src={flag} alt="" />
-              <select>
-                <option value="">EN</option>
-              </select>
-            </Link>
-            <Link to={!user && "/auth"}>
-              <div>
-                {user ? (
-                  <>
-                    <p>Hello, {user?.email?.split("@")[0]}</p>
-                    <span onClick={() => auth.signOut()}>Sign out</span>
-                  </>
-                ) : (
-                  <>
-                    <p>Sign In</p>
-                    <span>Account & Lists</span>
-                  </>
-                )}
-              </div>
-            </Link>
-            <Link to="/orders">
-              <p>Returns</p>
-              <span>& Orders</span>
-            </Link>
-            <Link to="/cart" className={classes.cart}>
-              <LiaCartArrowDownSolid size={35} />
-              <span>{totalItem}</span>
-            </Link>
+        <div>
+          {/* logo */}
+          <a href={logo} alt="amazon logo"></a>
+          {/* delivery */}
+          <span>
+            <SlLocationPin />
+          </span>
+          <div>
+            <p>Delived to</p>
+            <span>Ethiopia</span>
           </div>
         </div>
+        <div>
+          {/* search */}
+          <select name="" id="">
+            <option value="">All</option>
+          </select>
+          <input type="text" name="" id="" placeholder="search product" />
+        </div>
+        {/* icons */}
+        <FaSearch size={37} />
+
+        <div>
+          <div>
+            <img src={flag} alt="" />
+            <select name="" id="">
+              <option value="">EN</option>
+            </select>
+          </div>
+          {/* three components */}
+          <a href="">
+            <div>
+              <p>Sign In</p>
+              <span>Account and Lists</span>
+            </div>
+          </a>
+          {/* orders */}
+          <a href="">
+            <div>
+              <p>Returns</p>
+              <span>& Orders</span>
+            </div>
+          </a>
+          {/* cart */}
+          <a to={"/cart"}>
+            <LiaCartArrowDownSolid size={35} />
+            <span>0</span>
+          </a>
+        </div>
       </section>
-      <LowerHeader />
     </section>
   );
 };
