38b485fa8ee6f537278a2b81393f8ba30aa4b2de
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-09-25 19:29:41 -0400

    Update firebase.js

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firestore
 const firebaseConfig = {
-  apiKey: "AIzaSyDI-_M0hKTKp2izgUIkcKjbrR5KckEONhc",
-  authDomain: "clone-fa0b6.firebaseapp.com",
-  projectId: "clone-fa0b6",
-  storageBucket: "clone-fa0b6.firebasestorage.app",
-  messagingSenderId: "401897006760",
-  appId: "1:401897006760:web:84054a7eaecb6fee4b849d",
-  measurementId: "G-F0Q65Q435X",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
